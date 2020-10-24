43bb5209b508becbd7bcff81693474398ff1de78
A <@>
2020-10-24 12:41:28 -0800

    WPF

---
+++ FNAWPF/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using ExampleFNA;
+
+namespace FNAWPF
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Intensity
+        {
+            get { return GameBase.lum; }
+            set
+            {
+                GameBase.lum = value;
+                OnPropertyChanged("Intensity");
+            }
+        }
+
+
+        public void btnClear_Click()
+        {
+            GameBase.pts.Clear();
+            Intensity = 255;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ FNAWPF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ExampleFNA;
+
+namespace FNAWPF
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args)
+        {
+			
+            try
+            {
+
+                using (GameBase game = new GameBase())
+                    game.Run();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
