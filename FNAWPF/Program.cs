bf082914cdea36d4bbe7fcc85b2400b0795ceff4
A <@>
2020-10-24 14:18:36 -0800

    changes

--- FNAWPF/Program.cs
+++ FNAWPF/Program.cs
@@ -5,15 +5,14 @@ namespace FNAWPF
 {
     class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
+        [STAThread]
         public static void Main(string[] args)
         {
-			
+            MainWindow window = new MainWindow();
+            window.Show();
+
             try
             {
-
                 using (GameBase game = new GameBase())
                     game.Run();
 
